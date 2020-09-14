d4c888c1f253dbe666e5f1817cb2f92e5fcf567e
RK-05 <65751521+RK-05@users.noreply.github.com>
2020-09-14 19:21:25 +0530

    Update actions.py

--- actions/actions.py
+++ actions/actions.py
@@ -16,7 +16,6 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
-import requests
 
 
 #class ActionGetName(Action):
